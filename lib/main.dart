88a9db31231735b8b82c9a0ed39a7840f7c35e46
liguoying <15510224541@163com>
2019-03-27 15:31:39 +0800

    ListView && 红心

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // This sample shows adding an action to an [AppBar] that opens a shopping cart.
 
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -24,6 +25,7 @@ class MyStatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+//      backgroundColor: Colors.redAccent,
       appBar: AppBar(
         title: Text('Hello World'),
         actions: <Widget>[
@@ -36,6 +38,81 @@ class MyStatelessWidget extends StatelessWidget {
           ),
         ],
       ),
+      body: new Center(
+//        child: new Text("women"),
+        child: new RandomWords(),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+//    return RandomWordsState();// 一个 Text
+    return RandomListState(); // ListView
+  }
+}
+
+///一个 Text
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(
+      wordPair.asPascalCase,
+      style: TextStyle(color: Colors.white),
+    );
+  }
+}
+
+/// ListView
+class RandomListState extends State<RandomWords> {
+  final _suggestion = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildWidget();
+  }
+
+  Widget _buildWidget() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return new Divider();
+        }
+        final index = i ~/ 2;
+        if (index >= _suggestion.length) {
+          _suggestion.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestion[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair wordPair) {
+    final _alreadySaved = _saved.contains(wordPair);
+    return new ListTile(
+      title: new Text(
+        wordPair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        _alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: _alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (_alreadySaved) {
+            _saved.remove(wordPair);
+          } else {
+            _saved.add(wordPair);
+          }
+        });
+      },
     );
   }
 }
